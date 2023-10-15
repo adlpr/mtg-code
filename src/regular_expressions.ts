d04ae7dae774edb4c76994bd23d06d39b060dfe2
adlpr <amdelpriore@gmail.com>
2023-10-15 13:42:10 -0700

    minor regex fix

--- src/regular_expressions.ts
+++ src/regular_expressions.ts
@@ -2,6 +2,6 @@ export const cardLineRegExp: RegExp = /^(?:\/\/ *)?(?:\w+: )?(\d+) ([^\[]+)(?: \
 export const lineSplitterRegExp: RegExp = /\r?\n/g;
 export const cardNameReplaceExp: RegExp = /^((?:\/\/ *)?(?:\w+: )?\d+ )(?:[^\[]+\b)(.*)$/;
 export const cardCollNoReplaceExp: RegExp = /^((?:\/\/ *)?(?:\w+: )?\d+ [^\[]+ \[[^\] ]+)(?: [^\] ]+)?([ \]].*)$/;
-export const cardPrintedNameReplaceExp: RegExp = /^((?:\/\/ *)?(?:\w+: )?\d+ [^\[]+ \[[^\]=]+)(?: = [^\]]*)?\]$/;
+export const cardPrintedNameReplaceExp: RegExp = /^((?:\/\/ *)?(?:\w+: )?\d+ [^\[]+ \[[^\]=]+)(?: =[^\]]*)?\]$/;
 export const searchLineRegExp: RegExp = /^\/\/ *Search: *(.*?) *$/i;
 export const commentLineRegExp: RegExp = /^\/\/.*$/;
